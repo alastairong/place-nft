897a5afc463ce7938ef046336cdd5d4b86fa2497
Alastair Ong <alastair.ong@holo.host>
2023-02-27 18:38:50 +0000

    WIP

--- ui/src/place_nft/place/types.ts
+++ ui/src/place_nft/place/types.ts
@@ -36,3 +36,34 @@ export type PlaceSignal = {
 };
 
 export type EntryTypes = {};
+
+export type Placement = {
+  pixel: number;
+}
+
+export interface DestructuredPlacement {
+  x: number;
+  y: number;
+  colorIndex: number;
+}
+
+export interface Snapshot {
+  imageData: Uint8Array; // 2 x 4-bit pixels per u8
+  placementCount: number; // Number of placements in this bucket
+  bucketIndex: number; // Number of 'bucket_size_sec' since START.
+}
+
+export type DoublePixel = number;
+
+export interface GetAuthorRankInput {
+  author: AgentPubKey
+  bucketIndex: number
+}
+
+/** DEBUGGING API */
+export interface PlaceAtInput {
+  placement: DestructuredPlacement
+  bucketIndex: number
+}
+
+export const Root = "ROOT";
