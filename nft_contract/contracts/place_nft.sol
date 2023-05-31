8039a629e80783cc5daf55f00ffb267d972b0f23
Alastair Ong <alastair.ong@holo.host>
2023-05-31 15:57:21 +0100

    WIP

--- nft_contract/contracts/place_nft.sol
+++ nft_contract/contracts/place_nft.sol
@@ -6,11 +6,11 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract MyNFT is ERC721URIStorage {
+contract placeNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("MyNFT", "NFT") {}
+    constructor() ERC721("placeNFT", "NFT") {}
 
     function mintNFT(string memory badgeAction)
         public
