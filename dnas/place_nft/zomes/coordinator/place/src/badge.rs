87978d6b74fca65fe6b5a403db56910366f2fe7e
Alastair Ong <alastair.ong@holo.host>
2023-06-07 18:08:26 +0100

    hApp runs

--- dnas/place_nft/zomes/coordinator/place/src/badge.rs
+++ dnas/place_nft/zomes/coordinator/place/src/badge.rs
@@ -1,210 +1,210 @@
-// use hdk::prelude::*;
-// use place_integrity::*;
+use hdk::prelude::*;
+use place_integrity::*;
 
-// #[hdk_extern]
-// fn get_badge_action(_: ()) -> ExternResult<Option<ActionHash>> {
-//     // Search if they already have committed their badge. It should be on their chain
-//     // If it does, return the hash of the action
-//     // let ZomeInfo {id, ..} = zome_info()?; // There's only 1 app entry def in this zome
-//     // let badge_app_entry_type = AppEntryDef::new(0.into(), id, EntryVisibility::Public);
-//     if let Ok(records) = &query(ChainQueryFilter::new().entry_type(UnitEntryTypes::Badge.try_into()?)) {
-//         if records.is_empty() {
-//             Ok(None)
-//         } else {
-//             let badge_action = records[0].action_address().clone();
-//             Ok(Some(badge_action))
-//         }
-//     } else {
-//         Err(wasm_error!(WasmErrorInner::Guest(
-//             "Cannot deserialise into a badge".into()
-//         )))
-//     }
-// }
+#[hdk_extern]
+fn get_badge_action(_: ()) -> ExternResult<Option<ActionHash>> {
+    // Search if they already have committed their badge. It should be on their chain
+    // If it does, return the hash of the action
+    // let ZomeInfo {id, ..} = zome_info()?; // There's only 1 app entry def in this zome
+    // let badge_app_entry_type = AppEntryDef::new(0.into(), id, EntryVisibility::Public);
+    if let Ok(records) = &query(ChainQueryFilter::new().entry_type(UnitEntryTypes::Badge.try_into()?)) {
+        if records.is_empty() {
+            Ok(None)
+        } else {
+            let badge_action = records[0].action_address().clone();
+            Ok(Some(badge_action))
+        }
+    } else {
+        Err(wasm_error!(WasmErrorInner::Guest(
+            "Cannot deserialise into a badge".into()
+        )))
+    }
+}
 
-// #[hdk_extern]
-// fn get_badge(action_hash: ActionHash) -> ExternResult<Vec<u8>> {
-//     let maybe_record = get(action_hash, GetOptions::default())?;
+#[hdk_extern]
+fn get_badge(action_hash: ActionHash) -> ExternResult<Vec<u8>> {
+    let maybe_record = get(action_hash, GetOptions::default())?;
     
-//     let badge = match maybe_record {
-//         Some(record) => record.entry().to_app_option::<Badge>().unwrap().unwrap(),
-//         None => return Err(wasm_error!(WasmErrorInner::Guest(
-//             "No badge exists at that address".into()
-//         ))),
-//     };
+    let badge = match maybe_record {
+        Some(record) => record.entry().to_app_option::<Badge>().unwrap().unwrap(),
+        None => return Err(wasm_error!(WasmErrorInner::Guest(
+            "No badge exists at that address".into()
+        ))),
+    };
 
-//     Ok(badge.image_data)
-// }
+    Ok(badge.image_data)
+}
 
-// #[derive(Clone, Debug, Serialize, Deserialize, SerializedBytes)]
-// pub struct GenerateBadgeInput {
-//     eth_address: String,
-//     eth_signed_contents: String,
-// }
+#[derive(Clone, Debug, Serialize, Deserialize, SerializedBytes)]
+pub struct GenerateBadgeInput {
+    eth_address: String,
+    eth_signed_contents: String,
+}
 
-// #[hdk_extern]
-// fn generate_badge(input: GenerateBadgeInput) -> ExternResult<ActionHash> {
-//     // Get final snapshot
-//     let final_bucket: u32 = 24 * 12; // 12 buckets per hour and 24 hours in this game
+#[hdk_extern]
+fn generate_badge(input: GenerateBadgeInput) -> ExternResult<ActionHash> {
+    // Get final snapshot
+    let final_bucket: u32 = 24 * 12; // 12 buckets per hour and 24 hours in this game
 
-//     let final_snapshot_result: Result<Snapshot, WasmError> = call(
-//         CallTargetCell::Local,
-//         ZomeName("place".into()),
-//         FunctionName("get_snapshot_at".into()),
-//         None,
-//         final_bucket,
-//     )
-//     .and_then(|snapshot_call_result| match snapshot_call_result {
-//         ZomeCallResponse::Ok(response) => response
-//             .decode()
-//             .map_err(|err| wasm_error!(WasmErrorInner::Guest(err.into()))),
-//         _ => {
-//             error!(
-//                 "Place zome returned error when fetching final snapshot: {:?} \n",
-//                 snapshot_call_result
-//             );
-//             Err(wasm_error!(WasmErrorInner::Guest("Error fetching final snapshot".into())))
-//         }
-//     })
-//     .map_err(|e| {
-//         error!("Failed to call Place zome to fetch snapshot: {:?}", e);
-//         wasm_error!(WasmErrorInner::Guest("Error calling Place zome".into()))
-//     });
+    let final_snapshot_result: Result<Snapshot, WasmError> = call(
+        CallTargetCell::Local,
+        ZomeName("place".into()),
+        FunctionName("get_snapshot_at".into()),
+        None,
+        final_bucket,
+    )
+    .and_then(|snapshot_call_result| match snapshot_call_result {
+        ZomeCallResponse::Ok(response) => response
+            .decode()
+            .map_err(|err| wasm_error!(WasmErrorInner::Guest(err.into()))),
+        _ => {
+            error!(
+                "Place zome returned error when fetching final snapshot: {:?} \n",
+                snapshot_call_result
+            );
+            Err(wasm_error!(WasmErrorInner::Guest("Error fetching final snapshot".into())))
+        }
+    })
+    .map_err(|e| {
+        error!("Failed to call Place zome to fetch snapshot: {:?}", e);
+        wasm_error!(WasmErrorInner::Guest("Error calling Place zome".into()))
+    });
 
-//     let final_snapshot = final_snapshot_result?;
-//     debug!("Final snapshot: {:?}", final_snapshot);
+    let final_snapshot = final_snapshot_result?;
+    debug!("Final snapshot: {:?}", final_snapshot);
     
-//     // And count number of placements this user had and generate badge
-//     // let placement_app_entry_type = AppEntryDef::new(0.into(), 0.into(), EntryVisibility::Public); 
-//     if let Ok(records) = &query(ChainQueryFilter::new().entry_type(UnitEntryTypes::Placement.try_into()?)) {
-//         if records.len() == 0 {
-//             Err(wasm_error!(WasmErrorInner::Guest(
-//                 "Only users who have placed a placement can generate a badge".into()
-//             )))
-//         } else {
-//             let author = agent_info()?.agent_latest_pubkey;
-//             let badge = Badge::new(final_snapshot, records.len() as u32, &author.to_string(), input.eth_address, input.eth_signed_contents);
-//             let action_hash = publish_badge(badge)?;
-//             Ok(action_hash)
-//         }
-//     } else {
-//         Err(wasm_error!(WasmErrorInner::Guest(
-//             "Only users who have placed a placement can generate a badge".into()
-//         )))
-//     }
-// }
+    // And count number of placements this user had and generate badge
+    // let placement_app_entry_type = AppEntryDef::new(0.into(), 0.into(), EntryVisibility::Public); 
+    if let Ok(records) = &query(ChainQueryFilter::new().entry_type(UnitEntryTypes::Placement.try_into()?)) {
+        if records.len() == 0 {
+            Err(wasm_error!(WasmErrorInner::Guest(
+                "Only users who have placed a placement can generate a badge".into()
+            )))
+        } else {
+            let author = agent_info()?.agent_latest_pubkey;
+            let badge = Badge::new(final_snapshot, records.len() as u32, &author.to_string(), input.eth_address, input.eth_signed_contents);
+            let action_hash = publish_badge(badge)?;
+            Ok(action_hash)
+        }
+    } else {
+        Err(wasm_error!(WasmErrorInner::Guest(
+            "Only users who have placed a placement can generate a badge".into()
+        )))
+    }
+}
 
-// #[derive(Clone, Debug, Serialize, Deserialize, SerializedBytes)]
-// pub struct GenerateHrlInput {
-//     badge_action: ActionHash,
-// }
+#[derive(Clone, Debug, Serialize, Deserialize, SerializedBytes)]
+pub struct GenerateHrlInput {
+    badge_action: ActionHash,
+}
 
-// #[hdk_extern]
-// fn generate_hrl(input: GenerateHrlInput) -> ExternResult<String> {
+#[hdk_extern]
+fn generate_hrl(input: GenerateHrlInput) -> ExternResult<String> {
     
-//     if let Some(record) = get(input.badge_action.clone(), Default::default())? { // Confirms that this badge exists
+    if let Some(record) = get(input.badge_action.clone(), Default::default())? { // Confirms that this badge exists
         
-//         // Extract info for HRL
-//         let badge_result = record.entry.to_app_option()
-//             .map_err(|err| wasm_error!(WasmErrorInner::Guest(
-//                 err.into()
-//             )));
+        // Extract info for HRL
+        let badge_result = record.entry.to_app_option()
+            .map_err(|err| wasm_error!(WasmErrorInner::Guest(
+                err.into()
+            )));
         
-//         let badge: Badge = match badge_result {
-//             Ok(Some(badge)) => badge,
-//             Ok(None) => return Err(wasm_error!(WasmErrorInner::Guest(
-//                 "Something went wrong".into()
-//             ))),
-//             Err(err) => return Err(err),
-//         };
+        let badge: Badge = match badge_result {
+            Ok(Some(badge)) => badge,
+            Ok(None) => return Err(wasm_error!(WasmErrorInner::Guest(
+                "Something went wrong".into()
+            ))),
+            Err(err) => return Err(err),
+        };
 
-//         let eth_address = badge.eth_address;
+        let eth_address = badge.eth_address;
        
-//         let mut hrl: String = input.badge_action.to_string();
-//         hrl.push_str(&eth_address); // In future this should be a hash so it can't be reverse engineered        
-//         let hrl_anchor = get_anchor_typed_path(&hrl)?;
-//         // Create link from HRL to badge
-//         create_link(
-//             hrl_anchor.path_entry_hash()?, // use hrl as anchor
-//             input.badge_action.clone(),         // use entry hash as target
-//             links::HRLtoBadgeLink::link_type(),
-//             links::HRLtoBadgeLink::link_tag(),
-//         )?;
+        let mut hrl: String = input.badge_action.to_string();
+        hrl.push_str(&eth_address); // In future this should be a hash so it can't be reverse engineered        
+        let hrl_anchor = get_anchor_typed_path(&hrl)?;
+        // Create link from HRL to badge
+        create_link(
+            hrl_anchor.path_entry_hash()?, // use hrl as anchor
+            input.badge_action.clone(),         // use entry hash as target
+            links::HRLtoBadgeLink::link_type(),
+            links::HRLtoBadgeLink::link_tag(),
+        )?;
 
-//         // Create link from badge to HRL
-//         create_link(
-//             input.badge_action.clone(), // use host agent pubkey as base
-//             hrl_anchor.path_entry_hash()?,         // use entry hash as target
-//             links::BadgetoHRLLink::link_type(),
-//             links::BadgetoHRLLink::link_tag(),
-//         )?;
-//         Ok(hrl)
-//     } else {
-//         Err(wasm_error!(WasmErrorInner::Guest(
-//             "Badge not found".into()
-//         )))
-//     }
-// }
+        // Create link from badge to HRL
+        create_link(
+            input.badge_action.clone(), // use host agent pubkey as base
+            hrl_anchor.path_entry_hash()?,         // use entry hash as target
+            links::BadgetoHRLLink::link_type(),
+            links::BadgetoHRLLink::link_tag(),
+        )?;
+        Ok(hrl)
+    } else {
+        Err(wasm_error!(WasmErrorInner::Guest(
+            "Badge not found".into()
+        )))
+    }
+}
 
 
-// #[derive(Clone, Debug, Serialize, Deserialize, SerializedBytes)]
-// pub struct SaveNftInput {
-//     nft_id: String,
-//     contract_address: String,
-//     hrl: String,
-// }
+#[derive(Clone, Debug, Serialize, Deserialize, SerializedBytes)]
+pub struct SaveNftInput {
+    nft_id: String,
+    contract_address: String,
+    hrl: String,
+}
 
-// #[hdk_extern]
-// fn save_nft(input: SaveNftInput) -> ExternResult<ActionHash> {
-//     let nft_record = NftRecord::new(input.nft_id, input.contract_address);
-//     let action_hash = create_entry(EntryTypes::NftRecord(nft_record.clone()))?;
+#[hdk_extern]
+fn save_nft(input: SaveNftInput) -> ExternResult<ActionHash> {
+    let nft_record = NftRecord::new(input.nft_id, input.contract_address);
+    let action_hash = create_entry(EntryTypes::NftRecord(nft_record.clone()))?;
     
-//     // Create link from HRL to NFT
-//     let hrl_anchor = get_anchor_typed_path(&input.hrl)?;
-//     create_link(
-//         hrl_anchor.path_entry_hash()?,         
-//         action_hash.clone(),       
-//         links::BadgetoHRLLink::link_type(),
-//         links::BadgetoHRLLink::link_tag(),
-//     )?;
+    // Create link from HRL to NFT
+    let hrl_anchor = get_anchor_typed_path(&input.hrl)?;
+    create_link(
+        hrl_anchor.path_entry_hash()?,         
+        action_hash.clone(),       
+        links::BadgetoHRLLink::link_type(),
+        links::BadgetoHRLLink::link_tag(),
+    )?;
 
-//     Ok(action_hash)
-// }
+    Ok(action_hash)
+}
 
-// #[hdk_extern]
-// fn get_nft(hrl: String) -> ExternResult<Option<NftRecord>> { // retrieve the registered NFT for a given HRL
-//     let hrl_anchor = get_anchor_typed_path(&hrl)?;
-//     let links_result = get_links(
-//         hrl_anchor.path_entry_hash()?,         
-//         LinkTypes::HRLtoNftIdLink,
-//         None
-//     )?;
+#[hdk_extern]
+fn get_nft(hrl: String) -> ExternResult<Option<NftRecord>> { // retrieve the registered NFT for a given HRL
+    let hrl_anchor = get_anchor_typed_path(&hrl)?;
+    let links_result = get_links(
+        hrl_anchor.path_entry_hash()?,         
+        LinkTypes::HRLtoNftIdLink,
+        None
+    )?;
 
-//     if links_result.is_empty() {
-//         Ok(None)
-//     } else {
-//         let target: ActionHash = ActionHash::from(links_result[0].target.clone()).into(); // There should only be one link
-//         let maybe_record = get(target, GetOptions::default())?;
+    if links_result.is_empty() {
+        Ok(None)
+    } else {
+        let target: ActionHash = ActionHash::from(links_result[0].target.clone()).into(); // There should only be one link
+        let maybe_record = get(target, GetOptions::default())?;
 
-//         match maybe_record {
-//             Some(record) => {
-//                 let maybe_nft_record = record.entry().to_app_option::<NftRecord>()
-//                     .map_err(|err| wasm_error!(WasmErrorInner::Guest(err.into())))?;
+        match maybe_record {
+            Some(record) => {
+                let maybe_nft_record = record.entry().to_app_option::<NftRecord>()
+                    .map_err(|err| wasm_error!(WasmErrorInner::Guest(err.into())))?;
                     
-//                 Ok(maybe_nft_record)
-//             },
-//             None => Ok(None)
-//         }           
-//     }
-// }
+                Ok(maybe_nft_record)
+            },
+            None => Ok(None)
+        }           
+    }
+}
 
-// fn publish_badge(badge: Badge) -> ExternResult<ActionHash> {
-//     let action_hash = create_entry(EntryTypes::Badge(badge.clone()))?;
-//     let _entry_hash = hash_entry(badge)?;
-//     Ok(action_hash)
-// }
+fn publish_badge(badge: Badge) -> ExternResult<ActionHash> {
+    let action_hash = create_entry(EntryTypes::Badge(badge.clone()))?;
+    let _entry_hash = hash_entry(badge)?;
+    Ok(action_hash)
+}
 
-// fn get_anchor_typed_path(anchor: &str) -> ExternResult<TypedPath> {
-//     let typed_path = Path::from(anchor).typed(links::HRLtoBadgeLink::link_type())?;
-//     typed_path.ensure()?;
-//     Ok(typed_path)
-// }
+fn get_anchor_typed_path(anchor: &str) -> ExternResult<TypedPath> {
+    let typed_path = Path::from(anchor).typed(links::HRLtoBadgeLink::link_type())?;
+    typed_path.ensure()?;
+    Ok(typed_path)
+}
--- dnas/place_nft/zomes/integrity/place/src/badge.rs
+++ dnas/place_nft/zomes/integrity/place/src/badge.rs
@@ -1,8 +1,7 @@
 use hdi::prelude::*;
-// use image::{ImageBuffer, Rgba};
-// use imageproc::{drawing::draw_text_mut};
+use image::{ImageBuffer, Rgba};
+use imageproc::{drawing::draw_text_mut};
 use rusttype::{Font, Scale};
-use photon_rs::native::{open_image, save_image};
 use crate::snapshot::Snapshot;
 use crate::double_pixel::DoublePixel;
 
@@ -11,7 +10,7 @@ use crate::double_pixel::DoublePixel;
 pub struct PixelStruct {
    x: u32,
    y: u32,
-//    pixel: Rgba<u8>
+   pixel: Rgba<u8>
 }
 
 // Template constants
@@ -31,94 +30,94 @@ pub struct Badge {
    pub eth_signed_contents: String,
 }
 
-// impl Badge {
-//     pub fn new(final_snapshot: Snapshot, placement_count: u32, author: &str, eth_address: String, eth_signed_contents: String) -> Self {
-//       let mut img = image::load_from_memory(TEMPLATE_DATA).unwrap().to_rgba8();
+impl Badge {
+    pub fn new(final_snapshot: Snapshot, placement_count: u32, author: &str, eth_address: String, eth_signed_contents: String) -> Self {
+      let mut img = image::load_from_memory(TEMPLATE_DATA).unwrap().to_rgba8();
 
-//       // apply the snapshot image to the template output buffer
-//       let pixel_array = convert_snapshot_to_pixel_array(final_snapshot.image_data);
+      // apply the snapshot image to the template output buffer
+      let pixel_array = convert_snapshot_to_pixel_array(final_snapshot.image_data);
       
-//       for pixel in pixel_array.iter() {
-//          img.put_pixel(pixel.x + TEMPLATE_OFFSET.0, pixel.y + TEMPLATE_OFFSET.1, pixel.pixel);
-//       }
-//       let font = Font::try_from_bytes(FONT_DATA as &[u8]).unwrap();
-//       img = write_placement_count(placement_count, &font, img);
-//       img = write_author_name(author, &font, img);
+      for pixel in pixel_array.iter() {
+         img.put_pixel(pixel.x + TEMPLATE_OFFSET.0, pixel.y + TEMPLATE_OFFSET.1, pixel.pixel);
+      }
+      let font = Font::try_from_bytes(FONT_DATA as &[u8]).unwrap();
+      img = write_placement_count(placement_count, &font, img);
+      img = write_author_name(author, &font, img);
 
-//       Self {
-//          image_data: img.into_raw(),
-//          eth_address,
-//          eth_signed_contents, // we will check this in the validation
-//       }
-//     }
-// }
+      Self {
+         image_data: img.into_raw(),
+         eth_address,
+         eth_signed_contents, // we will check this in the validation
+      }
+    }
+}
 
-// fn convert_snapshot_to_pixel_array(snapshot: Vec<DoublePixel>) -> Vec<PixelStruct> {
+fn convert_snapshot_to_pixel_array(snapshot: Vec<DoublePixel>) -> Vec<PixelStruct> {
     
-//    // first unpack the doublepixels into normal uint4s representing the 16 colors
-//    let mut unpacked_array: Vec<u8> = Vec::new();
-//    for doublepixel in snapshot.iter() {
-//        unpacked_array.push(doublepixel.lower());
-//        unpacked_array.push(doublepixel.upper());
-//    }
+   // first unpack the doublepixels into normal uint4s representing the 16 colors
+   let mut unpacked_array: Vec<u8> = Vec::new();
+   for doublepixel in snapshot.iter() {
+       unpacked_array.push(doublepixel.lower());
+       unpacked_array.push(doublepixel.upper());
+   }
 
-//    // then convert the uint4s into PixelStructs 
-//    let mut pixel_array: Vec<PixelStruct> = Vec::new();
-//    for i in 0..unpacked_array.len() {
-//        let color = COLOR_PALETTE[unpacked_array[i] as usize];
-//        pixel_array.push(PixelStruct {
-//            x: i as u32 % 128,
-//            y: i as u32 / 128,
-//            pixel: hex_to_rgba(color)
-//        })
-//    }
-//    pixel_array
-// }
+   // then convert the uint4s into PixelStructs 
+   let mut pixel_array: Vec<PixelStruct> = Vec::new();
+   for i in 0..unpacked_array.len() {
+       let color = COLOR_PALETTE[unpacked_array[i] as usize];
+       pixel_array.push(PixelStruct {
+           x: i as u32 % 128,
+           y: i as u32 / 128,
+           pixel: hex_to_rgba(color)
+       })
+   }
+   pixel_array
+}
 
-// fn hex_to_rgba(hex: &str) -> Rgba<u8> {
-//    let r = u8::from_str_radix(&hex[1..3], 16).unwrap();
-//    let g = u8::from_str_radix(&hex[3..5], 16).unwrap();
-//    let b = u8::from_str_radix(&hex[5..7], 16).unwrap();
-//    let a = if hex.len() > 7 {
-//        u8::from_str_radix(&hex[7..9], 16).unwrap()
-//    } else {
-//        255
-//    };
-//    Rgba([r, g, b, a])
-// }
+fn hex_to_rgba(hex: &str) -> Rgba<u8> {
+   let r = u8::from_str_radix(&hex[1..3], 16).unwrap();
+   let g = u8::from_str_radix(&hex[3..5], 16).unwrap();
+   let b = u8::from_str_radix(&hex[5..7], 16).unwrap();
+   let a = if hex.len() > 7 {
+       u8::from_str_radix(&hex[7..9], 16).unwrap()
+   } else {
+       255
+   };
+   Rgba([r, g, b, a])
+}
 
-// fn write_placement_count(count: u32, font: &Font, mut image: ImageBuffer<Rgba<u8>, Vec<u8>>) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
-//    // Set the scale (font size)
-//    let scale = Scale { x: 11.0, y: 11.0 };
+fn write_placement_count(count: u32, font: &Font, mut image: ImageBuffer<Rgba<u8>, Vec<u8>>) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
+   // Set the scale (font size)
+   let scale = Scale { x: 11.0, y: 11.0 };
 
-//    // Set the position where the text will be drawn
-//    let x = PLACEMENT_COUNT_OFFSET.0 as i32;
-//    let y = PLACEMENT_COUNT_OFFSET.1 as i32;
+   // Set the position where the text will be drawn
+   let x = PLACEMENT_COUNT_OFFSET.0 as i32;
+   let y = PLACEMENT_COUNT_OFFSET.1 as i32;
 
-//    // Set the text color
-//    let color = Rgba([0u8, 0u8, 0u8, 255]);
+   // Set the text color
+   let color = Rgba([0u8, 0u8, 0u8, 255]);
 
-//    // Draw the text onto the ImageBuffer
-//    let count_text = count.to_string();
-//    draw_text_mut(&mut image, color, x, y, scale, font, &count_text);
-//    image
-// }
+   // Draw the text onto the ImageBuffer
+   let count_text = count.to_string();
+   draw_text_mut(&mut image, color, x, y, scale, font, &count_text);
+   image
+}
 
-// fn write_author_name(name: &str, font: &Font, mut image: ImageBuffer<Rgba<u8>, Vec<u8>>) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
-//    // Set the scale (font size)
-//    let scale = Scale { x: 12.0, y: 12.0 };
+fn write_author_name(name: &str, font: &Font, mut image: ImageBuffer<Rgba<u8>, Vec<u8>>) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
+   // Set the scale (font size)
+   let scale = Scale { x: 12.0, y: 12.0 };
 
-//    // Set the position where the text will be drawn
-//    let x = AUTHOR_OFFSET.0 as i32;
-//    let y = AUTHOR_OFFSET.1 as i32;
+   // Set the position where the text will be drawn
+   let x = AUTHOR_OFFSET.0 as i32;
+   let y = AUTHOR_OFFSET.1 as i32;
 
-//    // Set the text color
-//    let color = Rgba([0u8, 0u8, 0u8, 255]);
+   // Set the text color
+   let color = Rgba([0u8, 0u8, 0u8, 255]);
 
-//    // Draw the text onto the ImageBuffer
-//    draw_text_mut(&mut image, color, x, y, scale, font, name);
-//    image
-// }
+   // Draw the text onto the ImageBuffer
+   draw_text_mut(&mut image, color, x, y, scale, font, name);
+   image
+}
 
 const COLOR_PALETTE: [&str; 16] = [
    "#FFFFFF",
